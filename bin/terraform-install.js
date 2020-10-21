ccf43560ee47871c66018f40a16317e6c10b019d
ppbra <kaka020998@gmail.com>
2020-10-21 16:32:05 +0700

    edit verison

--- bin/terraform-install.js
+++ bin/terraform-install.js
@@ -6,7 +6,8 @@ const commander = require("commander");
 const { version } = require("commander");
 
 commander
-    .option("-o,--os <os type>","Choose OS: macos or linux! example: -o linux")
+    .option("-o,--os <os type>","Choose OS: macos or ubuntu! example: -o ubuntu")
+    .helpOption('-h, --HELP', 'read more information')
 commander.parse(process.argv);
 
 const os = !!commander.os?commander.os:"";
@@ -15,9 +16,9 @@ if(os=="macos"){
     shelljs.exec("curl https://releases.hashicorp.com/terraform/0.13.4/terraform_0.13.4_darwin_amd64.zip\
     --output terraform.zip && unzip terraform.zip -d /usr/local/bin")
 }else{
-    if(os=="linux"){
+    if(os=="ubuntu"){
         shelljs.exec("curl https://releases.hashicorp.com/terraform/0.13.4/terraform_0.13.4_linux_amd64.zip\
-    --output terraform.zip && unzip terraform.zip -d /usr/local/bin")
+    --output terraform.zip && sudo apt install unzip && sudo unzip terraform.zip -d /usr/local/bin")
     }else{
         console.log("Cannot find that os! Please try again!");
     }
